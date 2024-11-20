db5eb50805e936c7379944d31f32ff216bb5faac
paulinasolorzano <32572042+paulinasolorzano@users.noreply.github.com>
2024-11-20 13:19:14 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,19 +7,26 @@ self.addEventListener('install', event => {
     console.log('Service Worker activated');
     event.waitUntil(self.clients.claim());
   });
-  
-  self.addEventListener('message', event => {
-    if (event.data && event.data.action === 'simulatePush') {
+
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.action === 'simulatePush') {
+    if (Notification.permission === 'granted') {
       console.log('Simulating push notification...');
       self.registration.showNotification('Test Notification', {
         body: 'This is a simulated push notification!',
       });
+    } else {
+      console.error('No permission to show notifications');
     }
-  });
+  }
+});
+
   
   // Handle notification click event
   self.addEventListener('notificationclick', event => {
     console.log('Notification clicked:', event.notification);
     event.notification.close();
   });
-  
+
+
